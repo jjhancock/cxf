287761528420d96a625cbde247a49e049b10cbd2
Justin Hancock <justin.hancock@slalom.com>
2014-01-08 10:09:39 -0700

    JJH:  got Lists working with Jackson as the message body writer / JSON Provider.

--- cxf/src/main/java/cxf/rest/ExampleRestService.java
+++ cxf/src/main/java/cxf/rest/ExampleRestService.java
@@ -3,17 +3,23 @@
  */
 package cxf.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.xml.bind.annotation.XmlRootElement;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
 /**
  * @author justin.hancock
  *
  */
+@XmlRootElement
 @Path("/example")
 @Service("exampleRestService")
 public class ExampleRestService
@@ -32,7 +38,19 @@ public class ExampleRestService
 	@Produces("application/json")
 	public boolean isPrime(@PathParam("num") int num)
 	{
-		return (num % 1 == 0) == true;
+		return (num % 2 == 0) == true;
+	}
+	
+	@GET
+	@Path("/foo")
+	@Produces("application/json")
+	public List<Foo> foo()
+	{
+		List<Foo> myFoo = new ArrayList<Foo>();
+		myFoo.add(Foo.newInstance("duder", "el duderino"));
+		myFoo.add(Foo.newInstance("yes", "no"));
+		return myFoo;
+		
 	}
 
 }
---
+++ cxf/src/main/java/cxf/rest/Foo.java
@@ -0,0 +1,41 @@
+package cxf.rest;
+
+public class Foo
+{
+	private String name;
+	private String age;
+
+	public Foo()
+	{
+		super();
+	}
+	
+	public static Foo newInstance(String name, String age)
+	{
+		Foo result = new Foo();
+		result.setName(name);
+		result.setAge(age);
+		return result;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getAge()
+	{
+		return age;
+	}
+
+	public void setAge(String age)
+	{
+		this.age = age;
+	}
+
+}
