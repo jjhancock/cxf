3bf62ce5bf79e9380fadd1958ed2d98e66f4259d
Justin Hancock <justin.hancock@slalom.com>
2014-02-11 18:12:23 -0700

    JJH:  I can get all cities and go through the security filter and everything works as I would like...  EXCEPT, I can't bind a User Object, or anything else for that matter, to the method signature.  WTF?  It works on POST...

--- cxf/src/main/java/com/cxf/rest/impl/CityRestService.java
+++ cxf/src/main/java/com/cxf/rest/impl/CityRestService.java
@@ -5,13 +5,13 @@ package com.cxf.rest.impl;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 
 import com.cxf.domain.City;
@@ -39,8 +39,8 @@ public class CityRestService
 	
 	@GET
 	@Path("/all")
-	@Secured("ROLE_ADMIN")
 	@Produces(MediaType.APPLICATION_JSON_VALUE)
+	@Consumes(MediaType.APPLICATION_JSON_VALUE)
 	public List<City> getCities()
 	{
 		return cService.findAll();		
